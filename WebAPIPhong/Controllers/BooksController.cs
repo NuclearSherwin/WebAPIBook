2b858a7ff548bf611429164ab775daaf643c1338
Mai Dinh Phong <phongspacenasa@gmail.com>
2022-09-25 10:07:47 +0700

    create crud for book controller

--- WebAPIPhong/Controllers/BooksController.cs
+++ WebAPIPhong/Controllers/BooksController.cs
@@ -36,14 +36,39 @@ namespace WebAPIPhong.Controllers
             return _db.Books.Find(id);
         }
 
-        [HttpPost]
+        [HttpPost("create")]
         public IActionResult Create(Book bookInput)
         {
             _db.Books.Add(bookInput);
             _db.SaveChanges();
             return Ok(StatusCode(201));
-        } 
-        
-        
+        }
+
+        [HttpPut("update/{id:int}")]
+        // https:localhost:5001/api/books/update/{id}
+        public IActionResult Update([FromRoute] int id, Book bookInputNew)
+        {
+            var bookDb = _db.Books.Find(id);
+            
+            bookDb.Tittle = bookInputNew.Tittle;
+            bookDb.Author = bookInputNew.Author;
+            bookDb.Category = bookInputNew.Category;
+            bookDb.PageNum = bookInputNew.PageNum;
+
+            _db.Books.Update(bookDb);
+            _db.SaveChanges();
+
+            return StatusCode(200);
+        }
+
+        [HttpDelete("delete/{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var book = _db.Books.Find(id);
+            _db.Books.Remove(book);
+            _db.SaveChanges();
+
+            return StatusCode(200);
+        }
     }
 }
